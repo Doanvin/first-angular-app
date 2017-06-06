b27f282cdcdb1aaea9398ded29c229d633b1d34f
Donavin <donavinhannon@gmail.com>
2017-06-05 23:46:10 -0700

    Initial commit to GitHub

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by don on 6/5/17.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html'
+})
+
+export class ServerComponent {
+
+}
