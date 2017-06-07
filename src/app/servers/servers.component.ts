879eafd542234ab238ed6d9ecc66133b6ee98f1d
Donavin <donavinhannon@gmail.com>
2017-06-06 20:08:36 -0700

    Completed through Assignment , lecture 31

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  allowUpdateUserName = false;
+  serverCreationStatus = 'No new server was created!';
+  serverName = 'Test Server';
+  userName = 'Rocco';
+  updateUserNameStatus = 'User name not updated!';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+      this.allowUpdateUserName = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onNewUserName() {
+    this.updateUserNameStatus = 'User name updated successfully! New user name is ' + this.userName;
+  }
+
+  onUpdateUserName(event: Event) {
+    this.userName = (<HTMLInputElement>event.target).value;
+  }
 }
